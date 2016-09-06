70779432bb505183e7a0ad3586cb4702e66d54e8
fo40225 <fo40225@hotmail.com>
2016-09-07 01:13:30 +0800

    Begin writing shared UI code https://msdn.microsoft.com/en-us/library/mt679501.aspx#Anchor_2

---
+++ Xamarin.Forms/WeatherApp/WeatherApp/WeatherApp/WeatherPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace WeatherApp
+{
+    public partial class WeatherPage : ContentPage
+    {
+        public WeatherPage()
+        {
+            InitializeComponent();
+            this.Title = "Sample Weather App";
+            getWeatherBtn.Clicked += GetWeatherBtn_Clicked;
+
+            //Set the default binding to a default object for now
+            this.BindingContext = new Weather();
+        }
+
+        private async void GetWeatherBtn_Clicked(object sender, EventArgs e)
+        {
+            Weather weather = await Core.GetWeather("60601");
+            getWeatherBtn.Text = weather.Title;
+        }
+    }
+}
