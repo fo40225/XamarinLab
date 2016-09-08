03e79be2b6b61300535fe67efb714b56faceb535
fo40225 <fo40225@hotmail.com>
2016-09-08 23:32:02 +0800

    Add UI for UWP to the solution

---
+++ Xamarin.Native/WeatherApp/WeatherApp/WeatherApp.UWP/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+//空白頁項目範本收錄在 http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace WeatherApp.UWP
+{
+    /// <summary>
+    /// 可以在本身使用或巡覽至框架內的空白頁面。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void GetWeatherButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(zipCodeEntry.Text))
+            {
+                Weather weather = await Core.GetWeather(zipCodeEntry.Text);
+                locationText.Text = weather.Title;
+                tempText.Text = weather.Temperature;
+                windText.Text = weather.Wind;
+                visibilityText.Text = weather.Visibility;
+                humidityText.Text = weather.Humidity;
+                sunriseText.Text = weather.Sunrise;
+                sunsetText.Text = weather.Sunset;
+
+                weatherBtn.Content = "Search Again";
+            }
+        }
+    }
+}
