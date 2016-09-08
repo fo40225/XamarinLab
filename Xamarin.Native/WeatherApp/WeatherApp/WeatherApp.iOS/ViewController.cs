b74baf7eeb08e8b43fb51592556caa36109ddcfa
fo40225 <fo40225@hotmail.com>
2016-09-08 23:04:27 +0800

    Add UI for iOS to the solution https://msdn.microsoft.com/en-us/library/dn879698.aspx#Anchor_4

--- Xamarin.Native/WeatherApp/WeatherApp/WeatherApp.iOS/ViewController.cs
+++ Xamarin.Native/WeatherApp/WeatherApp/WeatherApp.iOS/ViewController.cs
@@ -4,30 +4,42 @@ using UIKit;
 
 namespace WeatherApp.iOS
 {
-	public partial class ViewController : UIViewController
-	{
-		int count = 1;
+    public partial class ViewController : UIViewController
+    {
+        int count = 1;
 
-		public ViewController (IntPtr handle) : base (handle)
-		{
-		}
+        public ViewController (IntPtr handle) : base (handle)
+        {
+        }
 
-		public override void ViewDidLoad ()
-		{
-			base.ViewDidLoad ();
-			// Perform any additional setup after loading the view, typically from a nib.
-			Button.AccessibilityIdentifier = "myButton";
-			Button.TouchUpInside += delegate {
-				var title = string.Format ("{0} clicks!", count++);
-				Button.SetTitle (title, UIControlState.Normal);
-			};
-		}
+        public override void ViewDidLoad ()
+        {
+            base.ViewDidLoad ();
 
-		public override void DidReceiveMemoryWarning ()
-		{
-			base.DidReceiveMemoryWarning ();
-			// Release any cached data, images, etc that aren't in use.
-		}
-	}
+            this.weatherBtn.Layer.BorderColor = UIColor.White.CGColor;
+            this.weatherBtn.Layer.BorderWidth = 1;
+        }
+
+        public override void DidReceiveMemoryWarning ()
+        {
+            base.DidReceiveMemoryWarning ();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        async partial void GetWeatherBtn_Click(UIButton sender)
+        {
+            if (!String.IsNullOrEmpty(this.zipCodeEntry.Text))
+            {
+                Weather weather = await Core.GetWeather(zipCodeEntry.Text);
+                locationText.Text = weather.Title;
+                tempText.Text = weather.Temperature;
+                windText.Text = weather.Wind;
+                visibilityText.Text = weather.Visibility;
+                humidityText.Text = weather.Humidity;
+                sunriseText.Text = weather.Sunrise;
+                sunsetText.Text = weather.Sunset;
+            }
+        }
+    }
 }
 
