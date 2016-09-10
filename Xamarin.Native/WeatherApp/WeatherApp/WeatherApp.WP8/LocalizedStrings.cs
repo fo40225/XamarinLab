2636dfa488e2522fc086f97b3daa44eea510b4a0
fo40225 <fo40225@hotmail.com>
2016-09-11 03:17:34 +0800

    Add UI for WP8 to the solution

---
+++ Xamarin.Native/WeatherApp/WeatherApp/WeatherApp.WP8/LocalizedStrings.cs
@@ -0,0 +1,20 @@
+﻿using WeatherApp.WP8.Resources;
+
+namespace WeatherApp.WP8
+{
+    /// <summary>
+    /// 提供字串資源的存取權。
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources
+        {
+            get
+            {
+                return _localizedResources;
+            }
+        }
+    }
+}
---
+++ Xamarin.Native/WeatherApp/WeatherApp/WeatherApp.WP8/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using WeatherApp.WP8.Resources;
+
+namespace WeatherApp.WP8
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // 建構函式
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void GetWeatherButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(zipCodeEntry.Text))
+            {
+                Weather weather = await Core.GetWeather(zipCodeEntry.Text);
+                locationText.Text = weather.Title;
+                tempText.Text = weather.Temperature;
+                windText.Text = weather.Wind;
+                visibilityText.Text = weather.Visibility;
+                humidityText.Text = weather.Humidity;
+                sunriseText.Text = weather.Sunrise;
+                sunsetText.Text = weather.Sunset;
+
+                weatherBtn.Content = "Search Again";
+            }
+        }
+    }
+}
